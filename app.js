9d7ac9a92a380558df6f3b8a9524b2ac07d0b452
Vincent Force <vforce@t2bh.fr>
2016-11-21 19:07:55 +0100

    Essai de version multi-canal multi-user

--- app.js
+++ app.js
@@ -5,15 +5,34 @@ var io = require("socket.io")(http);
 var Log = require("log");
   log = new Log("debug")
 var port = process.env.PORT || 3000;
-app.use(express.static(__dirname  + "/public"));
-app.get('/', function(req,res){
-  res.redirect('index.html');
+//app.use(express.static(__dirname  + "/public"));
+var router = express.Router();
+router.get('/emettre/:channel/:user', function(req, res) {
+    res.render('emettre.ejs', {channel: req.params.channel, user: req.params.user});
 });
-io.on('connection', function(socket){
-  socket.on('stream', function(image){
-    socket.broadcast.emit('stream', image)
+router.get('/visualiser/:channel', function(req, res) {
+    res.render('visualiser.ejs', {channel: req.params.channel});
+});
+router.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+app.use('/', router);
+// app.get('/', function(req,res){
+//   res.redirect('index.html');
+// });
+var channels = [];
+
+for (var ii=1; ii<=2; ii++) {
+  channels[ii] = io.of('/' + ii);
+  channels[ii].on('connection', function(socket){
+    for (var jj=1; jj<=3; jj++) {
+      socket.on('stream-' + jj, function(image){
+        socket.broadcast.emit('stream-' + jj, image)
+      });
+    }
   });
-});
+}
+
 http.listen(port, function(){
   log.info('Serveur à l\'écoute du port %s', port)
 });
