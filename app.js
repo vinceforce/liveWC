94236d8d5794120a5a15c328bda002bd0f2eea81
Vincent Force <vforce@t2bh.fr>
2016-11-30 14:40:16 +0100

    Prise en compte de la déconnexion pour supprimer la visualisation correspondante

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 router.get('/emettre/:room', function(req, res) {
     res.render('emettre.ejs', {room: req.params.room});
-});
-router.get('/visualiser/:room', function(req, res) {
+})
+.get('/visualiser/:room', function(req, res) {
     res.render('visualiser.ejs', {room: req.params.room});
-});
-router.get('/', function(req, res) {
+})
+.get('/', function(req, res) {
     res.render('index.ejs');
 });
-app.use('/', router).use(express.static(__dirname  + "/public"));
+app.use('/', router)
+.use(express.static(__dirname  + "/public"));
 var rooms = ['/room1', '/room2'];
 var roomsIO = [];
 
@@ -40,10 +41,16 @@ for (var ii=0; ii<=1; ii++) {
       //console.log("app broadcast emit stream");
       socket.broadcast.emit('stream-effect', {room: data.room, effectclass : data.effectclass, socketId: socket.id});
     });
+    socket.on('disconnect', function(){
+      console.log(socket.id + ' : déconnectée');
+      socket.broadcast.emit('stream-delete', {socketId: socket.id});
+      // roomIO.emit('stream-delete', {room: rooms[ii], socketId: socket.id});
+      console.log(socket.id + ' : message envoyé pour supprimer player');
+    });
   });
-  roomIO.on('disconnect', function(socket){
-    socket.broadcast.emit('stream-delete', {room: data.room, socketId: socket.id});
-  });
+  // roomIO.on('disconnect', function(socket){
+  //   socket.broadcast.emit('stream-delete', {room: data.room, socketId: socket.id});
+  // });
 
   roomsIO.push(roomIO);
 }
